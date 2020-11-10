536915b0f57a80603afc8807209a2d4e36f34dfd
opilane <opilane@HP-CZC6297B8C>
2020-11-10 12:30:36 +0200

    Ljal

--- Maakonnad/Maakonnad/Page1.xaml.cs
+++ Maakonnad/Maakonnad/Page1.xaml.cs
@@ -17,6 +17,38 @@ namespace Maakonnad
             Picker picker;
             Entry entry;
             Image image;
-        }
-    }
-}
+            {
+                Grid gr = new Grid
+                {
+                    RowDefinitions =
+                    {
+                        new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+                        new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+                        new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+                    },
+                    ColumnDefinitions =
+                    {
+                        new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
+                        new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) }
+                    }
+                };
+                picker = new Picker
+                {
+                    Title = "Maakonnad"
+                };
+                picker.Items.Add("Валгамаа");
+                picker.Items.Add("Вильяндимаа");
+                picker.Items.Add("Вырумаа");
+                picker.Items.Add("Ида-Вирумаа");
+                picker.Items.Add("Йыгевамаа");
+                picker.Items.Add("Ляэне-Вирумаа");
+                picker.Items.Add("Ляэнемаа");
+                picker.Items.Add("Пылвамаа");
+                picker.Items.Add("Пярнумаа");
+                picker.Items.Add("Рапламаа");
+                picker.Items.Add("Сааремаа");
+                picker.Items.Add("Тартумаа");
+                picker.Items.Add("Харьюмаа");
+                picker.Items.Add("Хийумаа");
+                picker.Items.Add("Ярвамаа");
+                gr.Children.Add
