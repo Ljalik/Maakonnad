7fdc5bad06d15c3b1d189091f5c293c40df92b22
opilane <opilane@HP-CZC6297B8C>
2020-11-16 17:32:05 +0200

    Ljalik

--- Maakonnad/Maakonnad/Page1.xaml.cs
+++ Maakonnad/Maakonnad/Page1.xaml.cs
@@ -35,7 +35,7 @@ namespace Maakonnad
                 };
                 picker = new Picker
                 {
-                    Title = "Уезды"
+                    Title = "Уезды Эстонии"
                 };
                 picker.Items.Add("Валгамаа");
                 picker.Items.Add("Вильяндимаа");
@@ -77,10 +77,175 @@ namespace Maakonnad
                 gr.Children.Add(stol, 0, 0);
 
                 stol.SelectedIndexChanged += Stol_SelectedIndexChanged;
-                
-                
-                
-                
+                entry = new Entry
+                {
+                    Text = "Уезд"
+                };
+                gr.Children.Add(entry, 1, 1);
+
+                img = new Image
+                {
+                    Source = "harju.jpg"
+                };
+                {
+                    if (stol.SelectedIndex == 0)
+                    {
+                        img.Source = "valga.png";
+                        picker.Title = picker.Items[stol.SelectedIndex];
+                    }
+                    else if (stol.SelectedIndex == 1)
+                    {
+                        img.Source = "viljandi.jpg";
+                        picker.Title = picker.Items[stol.SelectedIndex];
+                    }
+                    else if (stol.SelectedIndex == 2)
+                    {
+                        img.Source = "voruma.jpg";
+                        picker.Title = picker.Items[stol.SelectedIndex];
+                    }
+                    else if (stol.SelectedIndex == 3)
+                    {
+                        img.Source = "ida.jpg";
+                        picker.Title = picker.Items[stol.SelectedIndex];
+                    }
+                    else if (stol.SelectedIndex == 4)
+                    {
+                        img.Source = "jigi.jpg";
+                        picker.Title = picker.Items[stol.SelectedIndex];
+                    }
+                    else if (stol.SelectedIndex == 5)
+                    {
+                        img.Source = "laene.jpg";
+                        picker.Title = picker.Items[stol.SelectedIndex];
+                    }
+                    else if (stol.SelectedIndex == 6)
+                    {
+                        img.Source = "laenemaa.jpg";
+                        picker.Title = picker.Items[stol.SelectedIndex];
+                    }
+                    else if (stol.SelectedIndex == 7)
+                    {
+                        img.Source = "polvama.jpg";
+                        picker.Title = picker.Items[stol.SelectedIndex];
+                    }
+                    else if (stol.SelectedIndex == 8)
+                    {
+                        img.Source = "parnu.jpg";
+                        picker.Title = picker.Items[stol.SelectedIndex];
+                    }
+                    else if (stol.SelectedIndex == 9)
+                    {
+                        img.Source = "rapla.jpg";
+                        picker.Title = picker.Items[stol.SelectedIndex];
+                    }
+                    else if (stol.SelectedIndex == 10)
+                    {
+                        img.Source = "sarema.jpg";
+                        picker.Title = picker.Items[stol.SelectedIndex];
+                    }
+                    else if (stol.SelectedIndex == 11)
+                    {
+                        img.Source = "tartu.jpg";
+                        picker.Title = picker.Items[stol.SelectedIndex];
+                    }
+                    else if (stol.SelectedIndex == 12)
+                    {
+                        img.Source = "harju.jpg";
+                        picker.Title = picker.Items[stol.SelectedIndex];
+                    }
+                    else if (stol.SelectedIndex == 13)
+                    {
+                        img.Source = "hiju.jpg";
+                        picker.Title = picker.Items[stol.SelectedIndex];
+                    }
+                    else if (stol.SelectedIndex == 14)
+                    {
+                        img.Source = "jarva.jpg";
+                        picker.Title = picker.Items[stol.SelectedIndex];
+                    }
+                }
+                {
+                    if (picker.SelectedIndex == 0)
+                    {
+                        img.Source = "valga.png";
+                        stol.Title = stol.Items[stol.SelectedIndex];
+                    }
+                    else if (picker.SelectedIndex == 1)
+                    {
+                        img.Source = "viljandi.jpg";
+                        stol.Title = stol.Items[stol.SelectedIndex];
+                    }
+                    else if (picker.SelectedIndex == 2)
+                    {
+                        img.Source = "voruma.jpg";
+                        stol.Title = stol.Items[stol.SelectedIndex];
+                    }
+                    else if (picker.SelectedIndex == 3)
+                    {
+                        img.Source = "ida.jpg";
+                        stol.Title = stol.Items[stol.SelectedIndex];
+                    }
+                    else if (picker.SelectedIndex == 4)
+                    {
+                        img.Source = "jigi.jpg";
+                        stol.Title = stol.Items[stol.SelectedIndex];
+                    }
+                    else if (picker.SelectedIndex == 5)
+                    {
+                        img.Source = "laene.jpg";
+                        stol.Title = stol.Items[stol.SelectedIndex];
+                    }
+                    else if (picker.SelectedIndex == 6)
+                    {
+                        img.Source = "laenemaa.jpg";
+                        stol.Title = stol.Items[stol.SelectedIndex];
+                    }
+                    else if (picker.SelectedIndex == 7)
+                    {
+                        img.Source = "polvama.jpg";
+                        stol.Title = stol.Items[stol.SelectedIndex];
+                    }
+                    else if (picker.SelectedIndex == 8)
+                    {
+                        img.Source = "parnu.jpg";
+                        stol.Title = stol.Items[stol.SelectedIndex];
+                    }
+                    else if (picker.SelectedIndex == 9)
+                    {
+                        img.Source = "rapla.jpg";
+                        stol.Title = stol.Items[stol.SelectedIndex];
+                    }
+                    else if (picker.SelectedIndex == 10)
+                    {
+                        img.Source = "sarema.jpg";
+                        stol.Title = stol.Items[stol.SelectedIndex];
+                    }
+                    else if (picker.SelectedIndex == 11)
+                    {
+                        img.Source = "tartu.jpg";
+                        stol.Title = stol.Items[stol.SelectedIndex];
+                    }
+                    else if (picker.SelectedIndex == 12)
+                    {
+                        img.Source = "tartu.jpg";
+                        stol.Title = stol.Items[stol.SelectedIndex];
+                    }
+                    else if (picker.SelectedIndex == 13)
+                    {
+                        img.Source = "harju.jpg";
+                        stol.Title = stol.Items[stol.SelectedIndex];
+                    }
+                    else if (picker.SelectedIndex == 14)
+                    {
+                        img.Source = "hiju.jpg";
+                        stol.Title = stol.Items[stol.SelectedIndex];
+                    }
+                    else if (picker.SelectedIndex == 15)
+                    {
+                        img.Source = "jarva.jpg";
+                        stol.Title = stol.Items[stol.SelectedIndex];
+                    }
+                }
                 {
                     throw new NotImplementedException();
                 }
@@ -97,3 +262,4 @@ namespace Maakonnad
             throw new NotImplementedException();
         }
     }
+}
